f010157db65747b6694f5e489720f8ff0e615c28
liaoyi <3390351358@qq.com>
2025-05-22 16:07:15 +0800

    shiyan

--- src/main/java/love/linyi/controller/Code.java
+++ src/main/java/love/linyi/controller/Code.java
@@ -8,7 +8,6 @@ public class Code {
 
     public static final Integer SAVE_ERR=20010;
     public static final Integer DELETE_ERR=20020;
-    public static final Integer UPDATE_ERR=20030;
     public static final Integer GET_ERR=20040;
 //    public static final String host="http://linyi.love:25565/";
 //    public static final String host="http://localhost/";
